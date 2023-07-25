d4668a8ab11e0af05f4003fb0a9916ed8c555021
JohnPeterRicaro <johnpeterricaro@gmail.com>
2023-07-26 04:44:29 +0800

    Updated favicon moved to diff. folder

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -15,7 +15,7 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta charset="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <link rel="shortcut icon" href="/assets/favicon-32x32.png" />
+        <link rel="shortcut icon" href="/favicon.png" />
         <title>Calculator Main App</title>
     </Head>
       <ContextProvider>
